6f6593462f1694ea5320c352740b212bebe87c1f
AndiHOK91 <60509228+AndiHOK91@users.noreply.github.com>
2026-09-08 16:28:11 +0200

    Use selectable entity for battery capacity

--- custom_components/akkusteuerung_sma/const.py
+++ custom_components/akkusteuerung_sma/const.py
@@ -7,8 +7,12 @@ DEFAULT_NAME = "SMA Akku Steuerung"
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_MODBUS_ID = "modbus_id"
-CONF_BATTERY_CAPACITY = "battery_capacity"
+CONF_BATTERY_CAPACITY_ENTITY = "battery_capacity_entity"
+CONF_BATTERY_SOC_ENTITY = "battery_soc_entity"
+CONF_BATTERY_POWER_ENTITY = "battery_power_entity"
+CONF_PV_POWER_ENTITY = "pv_power_entity"
+CONF_GRID_POWER_ENTITY = "grid_power_entity"
+CONF_HOUSE_CONSUMPTION_ENTITY = "house_consumption_entity"
 
 DEFAULT_PORT = 502
 DEFAULT_MODBUS_ID = 3
-DEFAULT_BATTERY_CAPACITY = 10
