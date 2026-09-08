0c5f01a0f9d642e9bf7b4e355b780e6d9dc7b18c
AndiHOK91 <60509228+AndiHOK91@users.noreply.github.com>
2026-09-08 16:37:41 +0200

    Initialize coordinator for integration

--- custom_components/akkusteuerung_sma/__init__.py
+++ custom_components/akkusteuerung_sma/__init__.py
@@ -4,13 +4,34 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .coordinator import SMAAkkuCoordinator
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up the integration from a config entry."""
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {}
+    coordinator = SMAAkkuCoordinator(hass, entry)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
+        "coordinator": coordinator,
+    }
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        ["sensor", "number", "select"],
+    )
+
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload the integration."""
-    hass.data[DOMAIN].pop(entry.entry_id, None)
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry,
+        ["sensor", "number", "select"],
+    )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+
+    return unload_ok
