091a8a5e6f9474a373a558f0cef9c9a53eed955d
AndiHOK91 <60509228+AndiHOK91@users.noreply.github.com>
2026-09-08 16:31:02 +0200

    Add SMA inverter configuration options

--- custom_components/akkusteuerung_sma/const.py
+++ custom_components/akkusteuerung_sma/const.py
@@ -1,12 +1,14 @@
 """Constants for SMA battery control integration."""
 
 DOMAIN = "akkusteuerung_sma"
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 DEFAULT_NAME = "SMA Akku Steuerung"
 
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_MODBUS_ID = "modbus_id"
+CONF_SLAVE = "slave"
+
 CONF_BATTERY_CAPACITY_ENTITY = "battery_capacity_entity"
 CONF_BATTERY_SOC_ENTITY = "battery_soc_entity"
 CONF_BATTERY_POWER_ENTITY = "battery_power_entity"
@@ -14,5 +16,10 @@ CONF_PV_POWER_ENTITY = "pv_power_entity"
 CONF_GRID_POWER_ENTITY = "grid_power_entity"
 CONF_HOUSE_CONSUMPTION_ENTITY = "house_consumption_entity"
 
+CONF_INVERTER_LIMIT_ENTITY = "inverter_limit_entity"
+CONF_INVERTER_STATUS_ENTITY = "inverter_status_entity"
+CONF_INVERTER_TEMPERATURE_ENTITY = "inverter_temperature_entity"
+
 DEFAULT_PORT = 502
 DEFAULT_MODBUS_ID = 3
+DEFAULT_SLAVE = 3
