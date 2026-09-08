fcb8c0717f7148489f5d105d64d786e1853276ef
AndiHOK91 <60509228+AndiHOK91@users.noreply.github.com>
2026-09-08 16:23:47 +0200

    Expand integration constants

--- custom_components/akkusteuerung_sma/const.py
+++ custom_components/akkusteuerung_sma/const.py
@@ -3,3 +3,12 @@
 DOMAIN = "akkusteuerung_sma"
 VERSION = "0.1.0"
 DEFAULT_NAME = "SMA Akku Steuerung"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_MODBUS_ID = "modbus_id"
+CONF_BATTERY_CAPACITY = "battery_capacity"
+
+DEFAULT_PORT = 502
+DEFAULT_MODBUS_ID = 3
+DEFAULT_BATTERY_CAPACITY = 10
